8a6183b7469985eb8ae43f956bd00f7a30f62a66
Jon Lesser <jonlesser@gmail.com>
2012-07-14 19:31:54 -0700

    .

---
+++ appengine/main.py
@@ -0,0 +1,45 @@
+import jinja2
+import json
+import os
+import webapp2
+
+from datetime import datetime, timedelta
+from owl2 import word_list, word_dict
+
+templates = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainHandler(webapp2.RequestHandler):
+  def get(self):
+    template = templates.get_template('index.html')
+    self.response.out.write(template.render())
+
+
+class ApiCheckHandler(webapp2.RequestHandler):
+  def get(self, word):
+    resp = (word in word_dict)
+    self.response.content_type = 'application/json'
+    self.response.cache_control.no_cache = None
+    self.response.cache_control.public = True
+    self.response.headers.add('Expires',
+        (datetime.utcnow() + timedelta(days=(3650))).strftime("%a, %d %b %Y %H:%M:%S GMT"))
+    self.response.out.write(json.dumps(resp))
+
+
+class ApiListHandler(webapp2.RequestHandler):
+  def get(self, letter):
+    resp = word_list[letter]
+    self.response.content_type = 'application/json'
+    self.response.cache_control.no_cache = None
+    self.response.cache_control.public = True
+    self.response.headers.add('Expires',
+        (datetime.utcnow() + timedelta(days=(3650))).strftime("%a, %d %b %Y %H:%M:%S GMT"))
+    self.response.out.write(json.dumps(resp))
+
+
+debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
+app = webapp2.WSGIApplication([
+  webapp2.Route(r'/', MainHandler, name='main'),
+  webapp2.Route(r'/api/check/<word:[a-zA-Z]{1,32}>', ApiCheckHandler, name='api_check'),
+  webapp2.Route(r'/api/list/<letter:[a-zA-Z]>', ApiListHandler, name='api_list'),
+], debug=debug)
