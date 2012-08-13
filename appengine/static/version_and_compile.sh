26f51e17d24cf40556425def208ced63c9a69236
Jon Lesser <jonlesser@gmail.com>
2012-08-12 20:31:54 -0700

    version folder instead of filename.

--- appengine/static/version_and_compile.sh
+++ appengine/static/version_and_compile.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# Get a timestamp to use for the version.
+# Get a timestamp to use for the version and make a new folder.
 DATE=`date -u +%Y-%m-%d-%H-%M-%S`
+echo "Timestamp will be: $DATE"
+mkdir compiled/$DATE
 
 # Compile JS.
-
 js/closure-library/closure/bin/build/closurebuilder.py \
   --root=js/ \
   --namespace="nfw.CheckForm" \
@@ -12,16 +13,16 @@ js/closure-library/closure/bin/build/closurebuilder.py \
   --compiler_jar=js/closure-compiler/compiler.jar \
   --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
   --compiler_flags="--generate_exports" \
-  --output_file=compiled/main-$DATE.js
+  --output_file=compiled/$DATE/main.js
 
 # Compress Css.
-echo 'Compressing CSS...'
+echo "Compressing CSS..."
 java -jar css/closure-stylesheets.jar \
-  --output-file compiled/main-$DATE.css \
+  --output-file compiled/$DATE/main.css \
   css/main.css
 
 # Update the css and js links and the app version.
-echo 'Updating files...'
+echo "Updating files..."
 sed -E -i '' s/[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}/$DATE/ \
   ../main.html \
   ../app.yaml
