f6a079b1173f95d36da9ff39966d4f479e6b2dd2
Jon Lesser <jonlesser@gmail.com>
2012-08-12 20:52:36 -0700

    .

--- appengine/static/version_and_compile.sh
+++ appengine/static/version_and_compile.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 # Get a timestamp to use for the version and make a new folder.
-DATE=`date -u +%Y-%m-%d-%H-%M-%S`
+DATE=`date -u +%Y%m%d%H%M%S`
 echo "Timestamp will be: $DATE"
 mkdir compiled/$DATE
 
 # Compile JS.
-js/closure-library/closure/bin/build/closurebuilder.py \
-  --root=js/ \
-  --namespace="nfw.CheckForm" \
-  --output_mode=compiled \
-  --compiler_jar=js/closure-compiler/compiler.jar \
-  --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
-  --compiler_flags="--generate_exports" \
-  --output_file=compiled/$DATE/main.js
+#js/closure-library/closure/bin/build/closurebuilder.py \
+#  --root=js/ \
+#  --namespace="nfw.CheckForm" \
+#  --output_mode=compiled \
+#  --compiler_jar=js/closure-compiler/compiler.jar \
+#  --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
+#  --compiler_flags="--generate_exports" \
+#  --output_file=compiled/$DATE/main.js
 
 # Compress Css.
 echo "Compressing CSS..."
@@ -23,6 +23,5 @@ java -jar css/closure-stylesheets.jar \
 
 # Update the css and js links and the app version.
 echo "Updating files..."
-sed -E -i '' s/[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}/$DATE/ \
-  ../main.html \
-  ../app.yaml
+sed -E -i '' "s|compiled/[0-9]{14}|compiled/$DATE|" ../main.html
+sed -E -i '' "s|[0-9]{14}|$DATE|" ../app.yaml
