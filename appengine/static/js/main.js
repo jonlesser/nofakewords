f03664b5f18da2bd2b58af53b7e13b1ff1bb6d09
Jon Lesser <jonlesser@gmail.com>
2012-08-12 23:49:06 -0700

    First working version

--- appengine/static/js/main.js
+++ appengine/static/js/main.js
@@ -1,11 +1,15 @@
 goog.provide('nfw.CheckForm');
 
 goog.require('goog.dom');
+goog.require('goog.net.XhrIo');
+goog.require('goog.string');
 goog.require('goog.ui.Component');
 
 
 
 /**
+ * Component to handle the word check form.
+ * @param {goog.dom.DomHelper=} opt_domHelper Optional DOM helper.
  * @constructor
  * @export
  */
@@ -13,31 +17,86 @@ nfw.CheckForm = function(opt_domHelper) {
   goog.ui.Component.call(this, opt_domHelper);
 
   /**
-   * Event handler for this object.
    * @type {goog.events.EventHandler}
    * @private
    */
   this.eh_ = new goog.events.EventHandler(this);
 
-  console.log('constructed!');
+  /**
+   * @type {goog.net.XhrIo}
+   * @private
+   */
+  this.xhr_ = new goog.net.XhrIo();
+
+  /**
+   * @type {Element}
+   * @private
+   */
+  this.input_ = null;
+
+  /**
+   * @type {Element}
+   * @private
+   */
+  this.resultEl_ = null;
 };
 goog.inherits(nfw.CheckForm, goog.ui.Component);
 
 
+/**
+ * Attaches listeners and fetches some elements.
+ * @override
+ */
 nfw.CheckForm.prototype.enterDocument = function() {
   nfw.CheckForm.superClass_.enterDocument.call(this);
 
-  var submit = goog.dom.getElement('submit', this.getElement());
   this.eh_.listen(
       this.getElement(),
       goog.events.EventType.SUBMIT,
       this.submitHandler_);
+
+  this.eh_.listen(
+      this.xhr_,
+      goog.net.EventType.COMPLETE,
+      this.renderCheckResponse_);
+
+  this.input_ = goog.dom.getElement('input', this.getElement());
+  this.resultEl_ = goog.dom.getElement('result', this.getElement());
+
+  this.input_.select();
 };
 
 
+/**
+ * Handle check form submit.
+ * @param {!goog.events.Event} e Form submit event.
+ * @private
+ */
 nfw.CheckForm.prototype.submitHandler_ = function(e) {
   e.preventDefault();
-  console.log('submit happened');
+  if (/^[a-zA-Z]+$/.test(this.input_.value)) {
+    this.xhr_.send('/api/check/' + input.value.toLowerCase());
+  } else {
+    this.renderCheckResponse_(null);
+  }
+};
+
+
+/**
+ * Sets state for good and bad words from Xhr response. Can also be called
+ * with a null param to simulate a bad word without Xhr.
+ * @param {goog.events.Event} e Xhr complete event or null.
+ * @private
+ */
+nfw.CheckForm.prototype.renderCheckResponse_ = function(e) {
+  var valid = e ? !!e.target.getResponseJson() : false;
+  console.log(valid);
+  var text = goog.string.htmlEscape(this.input_.value) + ' is not a word.';
+  if (valid) {
+    text = goog.string.htmlEscape(this.input_.value) + ' is a word!';
+  }
+  this.resultEl_.innerHTML = text;
+  this.input_.select();
 };
 
 
