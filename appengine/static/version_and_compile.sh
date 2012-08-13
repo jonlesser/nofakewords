e2b376466d51fc413924351bb6a8d26c5b44f74b
Jon Lesser <jonlesser@gmail.com>
2012-08-12 20:20:10 -0700

    Adding compile script

---
+++ appengine/static/version_and_compile.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Get a timestamp to use for the version.
+DATE=`date -u +%Y-%m-%d-%H-%M-%S`
+
+# Compile JS.
+
+js/closure-library/closure/bin/build/closurebuilder.py \
+  --root=js/ \
+  --namespace="nfw.CheckForm" \
+  --output_mode=compiled \
+  --compiler_jar=js/closure-compiler/compiler.jar \
+  --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
+  --compiler_flags="--generate_exports" \
+  --output_file=compiled/main-$DATE.js
+
+# Compress Css.
+echo 'Compressing CSS...'
+java -jar css/closure-stylesheets.jar \
+  --output-file compiled/main-$DATE.css \
+  css/main.css
+
+# Update the css and js links and the app version.
+echo 'Updating files...'
+sed -E -i '' s/[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}/$DATE/ \
+  ../main.html \
+  ../app.yaml
